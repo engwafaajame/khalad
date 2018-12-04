812caad184714f6df7e54d13adc950c1f92d3158
engwafaajame <wafaa_200066@hotmail.com>
2018-12-04 16:38:42 +0200

    Update MainActivity.java

--- app/src/main/java/com/emuniapp/myapplication/MainActivity.java
+++ app/src/main/java/com/emuniapp/myapplication/MainActivity.java
@@ -37,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 }).addstudent(id.getText().toString(),name.getText().toString());
+                 id.setText("");
+                name.setText("");
 
 
             }
